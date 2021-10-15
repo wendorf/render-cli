800861a4cc13493a0f5e6e69de35a6cd48910cb6
Dan Wendorf <dan@render.com>
2021-10-15 10:22:52 -0700

    Get deploy

---
+++ cmd/deploy.go
@@ -0,0 +1,51 @@
+package cmd
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+
+	"github.com/renderinc/cli/pkg/http"
+	"github.com/renderinc/cli/pkg/table"
+	"github.com/spf13/cobra"
+)
+
+// deployCmd represents the deploy command
+var deployCmd = &cobra.Command{
+	Use:   "deploy",
+	Short: "Get deploy by service id and deploy id",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	Args: func(cmd *cobra.Command, args []string) error {
+		if len(args) != 2 {
+			return errors.New("provide exactly two arguments")
+		}
+
+		return nil
+	},
+	Run: func(cmd *cobra.Command, args []string) {
+		serviceId := args[0]
+		deployId := args[1]
+		jsonString, err := http.Request(fmt.Sprintf("services/%s/deploys/%s", serviceId, deployId))
+		if err != nil {
+			panic(err)
+		}
+
+		var deploy Deploy
+		if err := json.Unmarshal(jsonString, &deploy); err != nil {
+			panic(err)
+		}
+
+		if err := table.Print([]string{"Id", "Status", "CreatedAt", "FinishedAt"}, []Deploy{deploy}); err != nil {
+			panic(err)
+		}
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(deployCmd)
+}
